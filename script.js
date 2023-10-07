90dedd15829a6d87da9f90f770149bb76da34072
Annie Tran <tranthilananh105@gmail.com>
2023-10-07 17:16:44 +0300

    update time

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', 
 };
 
 const account2 = {
@@ -18,6 +30,18 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -35,6 +59,7 @@ const account4 = {
 };
 
 const accounts = [account1, account2, account3, account4];
+const accounts2 = [account1,account2]
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -202,3 +227,13 @@ labelBalance.addEventListener('click',function(){
   const movementsUI = Array.from(document.querySelectorAll('.movements__value'), el => Number(el.textContent.replace('€','')));
   console.log(movementsUI);
 });
+
+const bankDepositSum = accounts.map(acc => acc.movements).flat().filter(mov => mov >0).reduce((acc,mov)=>acc+mov,0);
+console.log(bankDepositSum); 
+
+const numDeposit1000 = accounts.flatMap(acc => acc.movements).reduce((count,cur) => cur >= 1000 ? count+1 : count,0);
+console.log(numDeposit1000);
+
+//Parsing
+console.log(Number.isNaN(20));
+  
